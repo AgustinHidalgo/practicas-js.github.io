12a03638f3c22edd468b70079f34805d3c8244f0
agustinhidalgo10 <agustinhidalgocef18@gmail.com>
2022-11-04 00:07:26 -0300

    practicando3

--- JS/index.js
+++ JS/index.js
@@ -1,15 +1,15 @@
-function productos (nombre, precio, categoria, img){
+function productos (nombre, precio, categoria, img, id){
     this.nombre = nombre
     this.precio = precio
     this.categoria = categoria
     this.img = img
+    this.id= id
 }
 
-let producto1 = new productos("Dulce de leche", 500, "crema", "./img/helado-de-dulce-de-leche-640x480.webp")
-let producto2 = new productos("Americana", 480, "crema", "./img/americana.webp")
-let producto3 = new productos("Frutilla", 450, "agua", "./img/frutilla.jpg")
-let producto4 = new productos("Durazno", 430, "agua", "./img/durazno.jpg")
-
+let producto1 = new productos("Dulce de leche", 500, "crema", "./img/helado-de-dulce-de-leche-640x480.webp",1)
+let producto2 = new productos("Americana", 480, "crema", "./img/americana.webp",2)
+let producto3 = new productos("Frutilla", 450, "agua", "./img/frutilla.jpg",3)
+let producto4 = new productos("Durazno", 430, "agua", "./img/durazno.jpg",4)
 
 let listaDeProductos = [producto1, producto2, producto3, producto4]
 let catalogo = document.getElementById("catalogo")
@@ -21,7 +21,7 @@ function creadorDeCards(listas){
 
         card.className = "card"
 
-        card.innerHTML = `<img src=${prod.img}> <h2> Sabor: ${prod.nombre}</h2> <p> Precio: $${prod.precio}</p>`
+        card.innerHTML = `<img src=${prod.img}> <h2> Sabor: ${prod.nombre}</h2> <p> Precio: $${prod.precio}</p> <button class="botonesStyle" mark="${prod.id}"> Comprar </button>`
         catalogo.append(card)
     }
 } 
@@ -33,22 +33,33 @@ let valorInput = document.getElementById("categoriaElegida")
 valorInput.addEventListener("change", ()=>{eleccionDeCategoria = valorInput.value})
 
 
+
 let botonDeFiltrado = document.getElementById("filtrar")
 
 botonDeFiltrado.addEventListener("click", filtradoProductos)
+
 let mostrarTodos = document.getElementById("volverATodos")
+
 mostrarTodos.addEventListener("click", ()=>{creadorDeCards(listaDeProductos)})
+
 function filtradoProductos(){
     let filtroNuevo = listaDeProductos.filter((prod) => prod.categoria == eleccionDeCategoria)
     creadorDeCards(filtroNuevo)
     if(filtroNuevo.length == 0){
         catalogo.innerHTML = "No tenemos ese producto todavia"
     }
-    
+    sessionStorage.setItem("usuarioCategoria", valorInput.value)
+
+let categoriaElegidaUsuario= sessionStorage.getItem("usuarioCategoria")
+
+console.log(categoriaElegidaUsuario)
 }
 
+localStorage.setItem("helados", JSON.stringify(listaDeProductos))
 
+let a = JSON.parse(localStorage.getItem("helados"))
 
+console.log(a)
 
 
 /* let eleccionDeSabor = prompt("Elija una categoria A) Crema B)Agua") */
